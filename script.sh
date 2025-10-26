d4d3bd5d33bb836842424669ac8ee1ac301a3135
akshay-eng <akshaysdeekshith@gmail.com>
2025-10-26 21:21:35 +0530

    added script2

--- script.sh
+++ script.sh
@@ -1,7 +1,8 @@
 mkdir -p /opt/duckdns
 cat >/opt/duckdns/update.sh <<'EOF'
 #!/bin/sh
-curl -fsS "https://www.duckdns.org/update?domains=<mystallion>&token=<YOUR_TOKEN>&ip=" \
+curl -fsS "https://www.duckdns.org/update?domains=<mystallion>&token=3008307a-5070-47b8-945d-fd6467aa81e0
+&ip=" \
   >>/var/log/duckdns.log 2>&1
 EOF
 chmod +x /opt/duckdns/update.sh
